b71d508c9c095fe1d3e6d98697f06f8f220a4982
aashikur <mdaashikur@gmail.com>
2025-04-08 12:42:16 +0600

    user load 10

--- custom-app/src/App.jsx
+++ custom-app/src/App.jsx
@@ -2,28 +2,41 @@
 import './App.css'
 import Counter from './Counter'
 import Bating from './Bating'
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
+import User from './Users'
+
+
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  // .then(res => res.json())
+  // .then(data => userData(data));
+
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res =>res.json());
+
+// const fetchFriends = async ()=>{
+// const res = await fetch('https://jsonplaceholder.typicode.com/users')
+// return res.json();
+// }
+
+
+
+const FetchUser = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
 function App() {
-  function handleClick(x = 0) {
-    const num = 7 + x;
-    alert(num);
-  };
 
+  
   return (
     <>
       <h1>Part 2</h1>
 
+      <Suspense fallback={<h3>Loading...</h3>  }>
+            <User FetchUser={FetchUser}></User>
+      </Suspense>
+
+
       <Counter></Counter> 
       <Bating></Bating>
-
-      <button onClick={handleClick}>CLick Here</button>
-      <button onClick={function handleClick2() {
-        alert("click2 button")
-      }}>CLick Here</button>
-
-      {/* {Send parameter } */}
-      <button onClick={() => handleClick(3)}>Send Parametter</button>
 
     </>
   )
---
+++ custom-app/src/Users.jsx
@@ -0,0 +1,19 @@
+import {use} from "react"
+export default function User({FetchUser}){
+
+    const theStyle = {
+      border: '1px solid white',
+      width: '100%',
+      padding: '10px',
+      margin: '5px auto',
+      borderRadius:'10px'
+    }
+
+    const users = use(FetchUser);
+    console.log(users);
+    return(<>
+      <div style={theStyle}>
+      <h3>USER : {users.length}</h3>
+      </div>
+    </>)
+}
